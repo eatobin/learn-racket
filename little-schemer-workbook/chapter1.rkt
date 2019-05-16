15400dabe293c79885a4d00777599fa857e09bd5
Eric Tobin <eatobin@gmail.com>
2019-05-16 16:50:07 -0500

    progress

--- little-schemer-workbook/chapter1.rkt
+++ little-schemer-workbook/chapter1.rkt
@@ -3,3 +3,14 @@
     (and (not (pair? x)) (not (null? x)))))
 
 (atom? '())
+(atom? (quote ()))
+(atom? 42)
+
+(null? '())
+(null? '(77))
+(zero? 0)
+(zero? 8)
+(define l1 (list '(+ 1 1) 7))
+(define l2 (list 7 '(+ 1 1)))
+(atom? (car l1))
+(atom? (car l2))
