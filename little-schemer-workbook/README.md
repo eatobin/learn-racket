3f742818b0b17850b5e4b4202c09da670c4ec09e
Eric Tobin <eatobin@gmail.com>
2019-05-16 15:51:52 -0500

    add workbook

---
+++ little-schemer-workbook/README.md
@@ -0,0 +1,3 @@
+# *The Little Schemer* Workbook
+
+(load "toc.rkt")
--- schemer.rkt
+++
@@ -1,9 +0,0 @@
-(module schemer racket
-  (provide atom?)
-
-  (define (atom? x)
-    (and (not (pair? x)) (not (null? x))))
-
-  (atom? '())
-  (atom? (quote atom))
-  (atom? 'atom))
