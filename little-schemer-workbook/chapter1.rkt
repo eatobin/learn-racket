e693cb34541115bc6b9887eb82bb8790e537c0ed
Eric Tobin <eatobin@gmail.com>
2019-05-19 16:38:16 -0500

    practice....

--- little-schemer-workbook/chapter1.rkt
+++ little-schemer-workbook/chapter1.rkt
@@ -11,8 +11,11 @@
 (null? '())
 (null? '(77))
 (zero? 0)
-(zero? 8)
+(zero? 9)
 (define l1 (list '(+ 1 1) 7))
 (define l2 (list 7 '(+ 1 1)))
 (atom? (car l1))
 (atom? (car l2))
+
+(define test
+  (= 0 0))
