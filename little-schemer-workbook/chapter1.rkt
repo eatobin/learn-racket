2cb36aa6aa22f57b31c4f653c168ecbd3a2df88d
Eric Tobin <eatobin@gmail.com>
2019-05-20 15:16:11 -0500

    racket/base

--- little-schemer-workbook/chapter1.rkt
+++ little-schemer-workbook/chapter1.rkt
@@ -1,20 +1,21 @@
 ; ,cd "~/lisp_projects/scheme/racket/learn-racket/little-schemer-workbook"
+; ,enter "chapter1.rkt"
 
-#lang racket
+#lang racket/base
 
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
-(atom? '())
-(atom? (quote ()))
-(atom? 42)
+; (atom? '())
+; (atom? (quote ()))
+; (atom? 42)
 
-(null? '())
-(null? '(77))
-(zero? 0)
-(zero? 9)
-(define l1 (list '(+ 1 1) 7))
-(define l2 (list 7 '(+ 1 1)))
-(atom? (car l1))
-(atom? (car l2))
+; (null? '())
+; (null? '(77))
+; (zero? 0)
+; (zero? 9)
+; (define l1 (list '(+ 1 1) 7))
+; (define l2 (list 7 '(+ 1 1)))
+; (atom? (car l1))
+; (atom? (car l2))
