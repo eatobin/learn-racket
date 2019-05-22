5ff01043a6da07d946b4b962a7ad42d876e072f0
Eric Tobin <eatobin@gmail.com>
2019-05-22 16:21:08 -0500

    add test files

---
+++ test2.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(+ (* 4 4) 7)
+
+(+ 1 1)
+
+(define my-add-one
+  (lambda (x)
+    (+ x 1)))
+
+(define my-add-two
+  (lambda (y)
+    (+ y 2)))
---
+++ test2.scm
@@ -0,0 +1,11 @@
+(* 4 4)
+
+(+ 1 1)
+
+(define my-add-one
+  (lambda (x)
+    (+ x 1)))
+
+(define my-add-two
+  (lambda (y)
+    (+ y 2)))
