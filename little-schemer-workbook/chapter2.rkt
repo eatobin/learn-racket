201a93353f5c7034867db0612d5707f6d83f23f7
Eric Tobin <eatobin@gmail.com>
2019-05-30 14:45:36 -0500

    progress

--- little-schemer-workbook/chapter2.rkt
+++ little-schemer-workbook/chapter2.rkt
@@ -2,3 +2,12 @@
 
 (define (atom? x)
   (and (not (pair? x)) (not (null? x))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l))(lat? (cdr l)))
+      (else #f))))
+
+(lat? '(bacon and eggs))
