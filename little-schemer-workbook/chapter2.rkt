72f4a342741abd4144d5cf4208260003921cbce9
Eric Tobin <eatobin@gmail.com>
2019-05-30 14:58:02 -0500

    progress

--- little-schemer-workbook/chapter2.rkt
+++ little-schemer-workbook/chapter2.rkt
@@ -11,3 +11,11 @@
       (else #f))))
 
 (lat? '(bacon and eggs))
+(lat? '(bacon (and eggs)))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
