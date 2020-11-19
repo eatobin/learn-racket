729e38a279b3ad76eb57d49b112ed212806bf3ae
Eric Tobin <eatobin@gmail.com>
2020-11-18 21:07:32 -0600

    add typed!!

---
+++ typed.rkt
@@ -0,0 +1,15 @@
+#lang typed/racket
+
+(define (id [z : Integer]) : Integer z)
+
+(define (mee [x : Number] [y : Number]) : Number
+  (+ x y))
+
+(define (adder [a : Integer] [b : Integer]) : Integer
+  (+ a b))
+
+(define (multi [x : Integer] [y : Integer]) : Integer
+  (* x y))
+
+(: suber (-> Integer Integer Integer))
+(define (suber x y) (- x y))
